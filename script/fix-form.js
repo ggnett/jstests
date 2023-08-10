76dfccf93fc428fb5ee1da399c12951f49c793d2
ggnett <matter1708@gmail.com>
2023-08-10 21:17:23 +0300

    making form...

--- script/fix-form.js
+++ script/fix-form.js
@@ -1,5 +1,6 @@
 let fix_button = document.querySelector(".info_button");
 let fix_form = document.querySelector(".fix-form");
+let chek = document.querySelector(".edit_name");
 fix_button.addEventListener("click", () => {
   fix_form.classList.add("visible_form");
 });
