ad8cdbea8258e0790a441e81cdd50f6a24f7074b
ggnett <matter1708@gmail.com>
2023-08-10 12:40:51 +0300

    start

---
+++ script/fix-form.js
@@ -0,0 +1,5 @@
+let fix_button = document.querySelector(".info_button");
+let fix_form = document.querySelector(".fix-form");
+fix_button.addEventListener("click", () => {
+  fix_form.classList.add("visible_form");
+});
